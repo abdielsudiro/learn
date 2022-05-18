ed3643689bb66f14cd8ded00e8b9c2c0c32cd11c
Abdiel Sudiro <abdiel.sudiro@gmail.com>
2022-05-18 17:01:12 +0700

    adding auth

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -2,12 +2,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Import Auth from the access-control subdirectory
+import "./access-control/Auth.sol";
+
 // Contract Box is a contract that let people store a value that can be later retrieved.
 contract Box {
     uint256 private _value;
+    Auth private _auth;
 
     // Emitted when the stored value changes
     event ValueChanged(uint256 value);
+
+    constructor() {
+        _auth = new Auth(msg.sender);
+    }
 
     // Stores a new value in the contract
     function store(uint256 value) public {
