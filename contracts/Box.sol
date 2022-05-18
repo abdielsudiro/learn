17d19bef0f90423d84bbd7709b1e573da6fabe0b
Abdiel Sudiro <abdiel.sudiro@gmail.com>
2022-05-18 17:25:24 +0700

    replace Auth contract with OpenZeppelin contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -2,28 +2,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Import Auth from the access-control subdirectory
-import "./access-control/Auth.sol";
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-// Contract Box is a contract that let people store a value that can be later retrieved.
-contract Box {
+// Make Box inherit from the Ownable contract
+contract Box is Ownable {
     uint256 private _value;
-    Auth private _auth;
 
-    // Emitted when the stored value changes
     event ValueChanged(uint256 value);
 
-    constructor() {
-        _auth = new Auth(msg.sender);
-    }
-
-    // Stores a new value in the contract
-    function store(uint256 value) public {
+    // The onlyOwner modifier restricts who can call the store function
+    function store(uint256 value) public onlyOwner {
         _value = value;
         emit ValueChanged(value);
     }
 
-    // Reads the last stored value
     function retrieve() public view returns (uint256) {
         return _value;
     }
